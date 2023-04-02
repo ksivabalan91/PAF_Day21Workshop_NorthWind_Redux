32dfff6d7ad64bf8fddc8fee0e376c4fa4cd0fb4
ksivabalan91 <ksivabalan.91@gmail.com>
2023-04-02 23:55:22 +0800

    add bash script

--- gitpush.sh
+++ gitpush.sh
@@ -1,13 +1,8 @@
-echo ----- git init -----
-git init
 echo ----- git add -----
 git add .
 echo Commit message
 read message
 echo ----- git commit-----
 git commit -m "$message"
-echo Enter repo url
-read url
-echo ----- git add remote and push-----
-git remote add origin $url
+echo ----- git push-----
 git push origin master
